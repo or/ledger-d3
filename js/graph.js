7e2f33d62b4cf4ab162fa265d4921cc3d12fca61
Oliver Runge <oliver.runge@gmail.com>
2017-10-01 23:32:28 +0200

    Add options to the graph

--- js/graph.js
+++ js/graph.js
@@ -1,12 +1,19 @@
+Date.prototype.addDays = function(days) {
+    var dat = new Date(this.valueOf());
+    dat.setDate(dat.getDate() + days);
+    return dat;
+};
+
 function load() {
     var width;
     var height;
     var graph_element_id = "#graph";
 
-    var svg = d3.select(graph_element_id)
-        .append("svg");
-    var graph;
-    var data;
+    var svg = d3.select(graph_element_id).append("svg");
+    var graph = svg.append("g").attr("class", "graph");
+    var optionLayer = svg.append("g").attr("class", "option-layer");
+    var rawData = null;
+    var data = null;
 
     var x = d3.scaleTime();
     var y = d3.scaleLinear();
@@ -14,10 +21,11 @@ function load() {
     var gX, gY;
 
     var day = d3.timeFormat("%Y-%m-%d");
+    var oldGraphIndex = null;
 
     var line = d3.line()
-        .curve(d3.curveBasis)
-        .x(function(d) { return x(d.date); })
+        .curve(d3.curveMonotone)
+        .x(function(d) { return x(d.aggregatedDate); })
         .y(function(d) { return y(d.cumulative_sum); });
 
     function zoomed() {
@@ -27,23 +35,15 @@ function load() {
         gY.call(yAxis.scale(d3.event.transform.rescaleY(y)));
     }
 
-    var zoom;
+    var minZoom = 0.1;
+    var zoom = d3.zoom()
+        .on("zoom", zoomed);
 
     var div = d3.select("body").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
-    var resize = function() {
-        width = $(graph_element_id).width();
-        height = $(graph_element_id).height();
-
-        svg.attr("width", width)
-           .attr("height", height);
-    };
-
     var graphObj = {};
-
-    var oldOnResize = d3.select(window).on("resize");
 
     var colorRange =  [
         "#1395ba",
@@ -57,6 +57,124 @@ function load() {
     var colorIndex = 0;
     var colorMap = {};
 
+    var optionGroups = [
+        {
+            x: 20,
+            y: 40,
+            name: "mode",
+            align: "left",
+            value: "line",
+            rebuildData: true,
+            options: [
+                {name: "line", icon: "\uf201"},
+                {name: "bar", icon: "\uf1fe"},
+                {name: "stack", icon: "\uf1fe"}
+            ]
+        },
+        {
+            x: 110,
+            y: 40,
+            name: "aggregation",
+            align: "left",
+            value: "day",
+            rebuildData: true,
+            options: [
+                {name: "day", icon: "\uf271"},
+                {name: "week", icon: "\uf271"},
+                {name: "month", icon: "\uf272"}
+            ]
+        },
+        {
+            x: 200,
+            y: 40,
+            name: "smoothing",
+            align: "left",
+            value: "smooth",
+            rebuildData: false,
+            options: [
+                {name: "raw", icon: "\uf272"},
+                {name: "smooth", icon: "\uf271"}
+            ]
+        },
+    ];
+
+    function optionIs(name, value) {
+        var optionGroup = optionGroups.filter(function(d) { return d.name == name; })[0];
+        return optionGroup.value == value;
+    }
+
+    function buildOptionGroup(optionGroup) {
+        optionGroup.options.forEach(function(d) {
+            d.optionGroup = optionGroup;
+        });
+
+        var controls = optionLayer.append("g")
+            .attr("class", "option-group")
+            .attr("opacity", 0)
+            .attr("transform", function(d, i) {
+                return "translate(" + 1000 + ",0)";
+            })
+        ;
+
+        var options = controls.selectAll('.option')
+                .data(optionGroup.options)
+                .enter()
+                .append("g")
+                .attr("class", "option")
+                .attr("transform", function(d, i) {
+                    var y = 15 + i * 20;
+                    return "translate(0," + y + ")";
+                })
+                .on("click", function(d, i) {
+                    d.optionGroup.value = d.name;
+                    if (d.optionGroup.rebuildData) {
+                        graphObj.buildData();
+                    }
+                    graphObj.update();
+                })
+        ;
+
+        options.append("text")
+            .attr("class", "no-select clickable")
+            .attr("text-anchor", function(d) {
+                if (d.optionGroup.align == "left") {
+                    return "start";
+                } else {
+                    return "end";
+                }
+            })
+            .attr("font-family", "FontAwesome")
+            .text(function(d) { return d.icon; })
+        ;
+
+        options.append("text")
+            .attr("class", "no-select clickable")
+            .attr("x", function(d) {
+                if (d.optionGroup.align == "left") {
+                    return 20;
+                } else {
+                    return -20;
+                }
+            })
+            .attr("text-anchor", function(d) {
+                if (d.optionGroup.align == "left") {
+                    return "start";
+                } else {
+                    return "end";
+                }
+            })
+            .text(function(d) { return d.name; })
+        ;
+
+        optionGroup.controls = controls;
+        optionGroup.controlOptions = options;
+    };
+
+    optionGroups.forEach(function(o) {
+        buildOptionGroup(o);
+    });
+
+
     var pickColor = function(name) {
         if (!(name in colorMap)) {
             colorMap[name] = colorIndex;
@@ -66,6 +184,8 @@ function load() {
         return colorRange[colorMap[name]];
     };
 
+    var oldOnResize = d3.select(window).on("resize");
+
     d3.select(window).on("resize", function() {
         if (typeof(oldOnResize) != "undefined") {
             oldOnResize();
@@ -74,48 +194,64 @@ function load() {
     });
 
     graphObj.onResize = function() {
-        resize();
+        width = $(graph_element_id).width();
+        height = $(graph_element_id).height();
+
+        svg.attr("width", width)
+            .attr("height", height);
+
         graphObj.update();
     };
 
     graphObj.load = function(path) {
         console.log("load...");
-        d3.json(path, function(raw_data) {
-            data = [];
-            var nameMap = {};
-            raw_data.forEach(function(d) {
-                if (!(d.commodity in nameMap)) {
-                    nameMap[d.commodity] = {
-                        key: d.commodity,
-                        cumulative_sum: 0,
-                        last: null,
-                        values: []
-                    };
-                }
-                var tmp = nameMap[d.commodity];
-
-                d.date = new Date(d.date);
-
-                if (tmp.last == null || day(tmp.last.date) != day(d.date)) {
-                    tmp.last = {
-                        date: d.date,
-                        amount: 0,
-                        cumulative_sum: 0,
-                        transactions: []
-                    };
-                    tmp.values.push(tmp.last);
-                }
-
-                d.amount = parseFloat(d.amount);
-                tmp.cumulative_sum += d.amount;
-                tmp.last.amount += d.amount;
-                tmp.last.cumulative_sum = tmp.cumulative_sum;
-                tmp.last.transactions.push(d);
-            });
-
-            data = Object.values(nameMap);
+        d3.json(path, function(receivedData) {
+            rawData = receivedData;
+            graphObj.buildData();
             graphObj.update();
         });
+    };
+
+    graphObj.buildData = function() {
+        var nameMap = {};
+        rawData.forEach(function(d) {
+            if (!(d.commodity in nameMap)) {
+                nameMap[d.commodity] = {
+                    key: d.commodity,
+                    cumulative_sum: 0,
+                    last: null,
+                    values: []
+                };
+            }
+            var tmp = nameMap[d.commodity];
+
+            d.date = new Date(d.date);
+
+            var aggregatedDate = d.date;
+            if (optionIs("aggregation", "week")) {
+                aggregatedDate = d.date.addDays(-(d.date.getDay() + 7 - 1) % 7);
+            } else if (optionIs("aggregation", "month")) {
+                aggregatedDate = d.date.addDays(-d.date.getDate() + 1);
+            }
+
+            if (tmp.last == null || day(tmp.last.aggregatedDate) != day(aggregatedDate)) {
+                tmp.last = {
+                    aggregatedDate: aggregatedDate,
+                    amount: 0,
+                    cumulative_sum: 0,
+                    transactions: []
+                };
+                tmp.values.push(tmp.last);
+            }
+
+            d.amount = parseFloat(d.amount);
+            tmp.cumulative_sum += d.amount;
+            tmp.last.amount += d.amount;
+            tmp.last.cumulative_sum = tmp.cumulative_sum;
+            tmp.last.transactions.push(d);
+        });
+
+        data = Object.values(nameMap);
     };
 
     graphObj.update = function() {
@@ -123,16 +259,15 @@ function load() {
             return;
         }
 
-        console.log(data);
         var xRange = [
             d3.min(data, function(d) {
                 return d3.min(d.values, function(e) {
-                    return e.date;
+                    return e.aggregatedDate;
                 });
             }),
             d3.max(data, function(d) {
                 return d3.max(d.values, function(e) {
-                    return e.date;
+                    return e.aggregatedDate;
                 });
             })
         ];
@@ -163,7 +298,7 @@ function load() {
 
         console.log(xRange, yRange, timelineWidth);
 
-        svg.selectAll("g").remove();
+        svg.selectAll("g.axis").remove();
         xAxis = d3.axisBottom(x)
             .ticks(d3.timeMonth.every(1))
             .tickFormat(d3.timeFormat("%b %Y"));
@@ -177,36 +312,76 @@ function load() {
             .attr("transform", "translate(" + width + " ,0)")
             .call(yAxis);
 
-        var minZoom = 0.1;
-        zoom = d3.zoom()
-            .scaleExtent([minZoom, 40])
+        zoom.scaleExtent([minZoom, 40])
             .translateExtent([[(-10 - width) / minZoom, -100 / minZoom],
-                              [(timelineWidth + width + 10) / minZoom, (height + 100) / minZoom]])
-            .on("zoom", zoomed);
+                              [(timelineWidth + width + 10) / minZoom, (height + 100) / minZoom]]);
 
-        graph = svg.append("g")
-            .attr("class", "graph");
+        if (optionIs("smoothing", "smooth")) {
+            line.curve(d3.curveMonotone);
+        } else {
+            line.curve(d3.curveLinear);
+        }
 
-        graphObj.update_data();
-        svg.call(zoom);
-    };
+        var t = d3.transition()
+            .duration(700)
+            .ease(d3.easeLinear);
 
-    graphObj.update_data = function() {
-        console.log("rendering...");
-        graph.selectAll(".line")
+        console.log("oldGraphIndex", oldGraphIndex);
+        if (oldGraphIndex !== null) {
+            graph
+                .selectAll(".line.line-" + oldGraphIndex)
+                .transition(t)
+                .attr("opacity", 0)
+                .remove();
+        } else {
+            oldGraphIndex = 1;
+        }
+
+        oldGraphIndex = 1 - oldGraphIndex;
+        graph.selectAll(".line.line-" + oldGraphIndex)
             .data(data)
             .enter()
             .append("path")
             .style("pointer-events", "none")
-            .attr("class", "line")
+            .attr("class", "line line-" + oldGraphIndex)
             .attr("fill", "none")
+            .attr("opacity", 0)
             .attr("stroke-width", 1)
             .attr("stroke", function(d) { return pickColor(d.key); })
+        ;
+
+        graph
+            .selectAll(".line.line-" + oldGraphIndex)
+            .data(data)
+            .transition(t)
+            .attr("opacity", 1)
             .attr("d", function(d) { return line(d.values); })
         ;
+
+        svg.selectAll(".option-group")
+            .data(optionGroups)
+            .transition(t)
+            .attr("opacity", 1)
+            .attr("transform", function(d, i) {
+                return "translate(" + d.x + "," + d.y + ")";
+            })
+        ;
+
+        svg.selectAll(".option")
+            .data(optionGroups.map(function(d) { return d.options; }).reduce(function(a, b) { return a.concat(b); }))
+            .transition(t)
+            .style("fill", function(d) {
+                if (d.optionGroup.value == d.name) {
+                    return "#000000";
+                }
+                return "#d0d0d0";
+            })
+        ;
+
+        svg.call(zoom);
     };
 
-    resize();
+    graphObj.onResize();
 
     graphObj.load("/data/assets.json");
 
